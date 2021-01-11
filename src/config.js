5b3cc1af21ec769b69a5cf99e2301df01d67b629
buzdolapci <tuneman333@live.com>
2021-01-11 07:39:15 +0000

    ist timzeone

--- src/config.js
+++ src/config.js
@@ -50,7 +50,7 @@ exports.localSleepTime = 3;
 exports.localWakeTime = 9;
 
 // Your desired timezone for displaying times
-exports.timeZone = "America/New_York";
+exports.timeZone = "Europe/Istanbul";
 
 // Time in seconds to ban users for Order 66
 exports.order66Time = 15;
